117b6ea78ba82c3b8d5bf1bf275a256f232c2d84
Dan Sherman <dvsherman@protonmail.com>
2019-01-29 15:25:10 -0700

    added load event on window

--- js/index.js
+++ js/index.js
@@ -1,12 +1,17 @@
 // Your code goes here
 
 // Event the first
+window.addEventListener("load", function(event) {
+    alert("All abord the Fun Bus!");
+  });
+
+// Event the second
 let logoHeading = document.querySelector('.logo-heading');
 logoHeading.addEventListener('mouseenter', event => {
     logoHeading.textContent = "Fun Bus is REALLY fun!";
 });
 
-// Event the second
+// Event the third
 logoHeading.addEventListener('mouseleave', event => {
     logoHeading.textContent = "Fun Bus";
 });
@@ -14,7 +19,7 @@ logoHeading.addEventListener('mouseleave', event => {
 let navLink = document.querySelectorAll('.nav-link');
 console.log(navLink);
 
-// Event the third
+// Event the fourth
 let growNavLinks = function(e) {
     e.currentTarget.style.fontSize = '2rem';
 }
@@ -30,5 +35,4 @@ for(var i = 0; i < navLink.length; i++){
     navLink[i].addEventListener('mouseleave', shrinkNavLinks, false);
 }
 
-// Event the fourth
-let 
+// Event the fifth
