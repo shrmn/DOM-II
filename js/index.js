66f66055f523681f58c19728fd00178a16b6ba1c
Dan Sherman <dvsherman@protonmail.com>
2019-01-29 15:42:34 -0700

    added resize event on window

--- js/index.js
+++ js/index.js
@@ -1,27 +1,26 @@
 // Your code goes here
 
 // Event the first
-window.addEventListener("load", function(event) {
+window.addEventListener('load', () => {    
     alert("All abord the Fun Bus!");
   });
 
 // Event the second
 let logoHeading = document.querySelector('.logo-heading');
-logoHeading.addEventListener('mouseenter', event => {
+logoHeading.addEventListener('mouseenter', () => {
     logoHeading.textContent = "Fun Bus is REALLY fun!";
 });
 
 // Event the third
-logoHeading.addEventListener('mouseleave', event => {
+logoHeading.addEventListener('mouseleave', () => {
     logoHeading.textContent = "Fun Bus";
 });
 
 let navLink = document.querySelectorAll('.nav-link');
-console.log(navLink);
 
 // Event the fourth
-let growNavLinks = function(e) {
-    e.currentTarget.style.fontSize = '2rem';
+let growNavLinks = (event) => {
+    event.currentTarget.style.fontSize = '2rem';
 }
 for(var i = 0; i < navLink.length; i++){
     navLink[i].addEventListener('click', growNavLinks, false);
@@ -36,3 +35,10 @@ for(var i = 0; i < navLink.length; i++){
 }
 
 // Event the fifth
+
+// NOTE: For some reason, the alert created by this cannot be dismissed.
+window.addEventListener('resize', () => {
+    alert('The window has been resized!')
+})
+
+// Event the sixth
