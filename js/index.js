2b663de5c22eb9f576e3d85d0b914d2a17a417d3
Dan Sherman <dvsherman@protonmail.com>
2019-01-29 14:58:21 -0700

    added mouseover event to .logo-heading

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+let logoHeading = document.querySelector('.logo-heading');
+console.log(logoHeading);
+
+logoHeading.addEventListener('mouseover', event => {
+    console.log(event.type);
+    logoHeading.textContent = "Fun Bus is REALLY fun!";
+});
