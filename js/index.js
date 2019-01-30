374a89a8772b3c98aceb3e80cd5f2ee2856a6dc8
Dan Sherman <dvsherman@protonmail.com>
2019-01-29 23:19:51 -0700

    attempting greensock animation

--- js/index.js
+++ js/index.js
@@ -96,3 +96,20 @@ images.forEach(image => image.addEventListener('auxclick', () => alert('You clic
 
 let funBusImg = document.querySelector('.intro img');
 funBusImg.addEventListener('dblclick', () => funBusImg.src = null);
+
+
+// Greensock animate text
+
+var tl = new TimelineLite, 
+    mySplitText = new SplitText("#quote", {type:"words,chars"}), 
+    chars = mySplitText.chars; //an array of all the divs that wrap each character
+
+TweenLite.set("#quote", {perspective:400});
+
+tl.staggerFrom(chars, 0.8, {opacity:0, scale:0, y:80, rotationX:180, transformOrigin:"0% 50% -50",  ease:Back.easeOut}, 0.01, "+=0");
+
+
+
+document.getElementById("animate").onclick = function() {
+  tl.restart();
+}
